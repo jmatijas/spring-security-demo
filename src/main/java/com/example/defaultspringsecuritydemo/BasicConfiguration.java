a1b077e318826d8bb06fc5a73f9afee6c2ef9d16
Josip Matijaš <josip.matijas@gmail.com>
2026-04-27 23:50:57 +0200

    Security configuration with InMemoryUserDetailsManager

---
+++ src/main/java/com/example/defaultspringsecuritydemo/BasicConfiguration.java
@@ -0,0 +1,58 @@
+package com.example.defaultspringsecuritydemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class BasicConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(
+//                        request -> request.anyRequest().authenticated()
+//                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/files/**").authenticated() // all roles authorized
+                        //.anyRequest().authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
+    }
+}
--- src/main/java/com/example/defaultspringsecuritydemo/HelloController.java
+++ src/main/java/com/example/defaultspringsecuritydemo/HelloController.java
@@ -1,13 +1,37 @@
 package com.example.defaultspringsecuritydemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/")
     String hello() {
+        log.info("hello()");
         return "Hello World!";
+    }
+
+    @GetMapping("/admin")
+    String helloAdmin() {
+        log.info("helloAdmin()");
+        return "Hello Admin!";
+    }
+
+    @GetMapping("/user")
+    String helloUser() {
+        log.info("helloUser()");
+        return "Hello User!";
+    }
+
+    @GetMapping("/files/{*filePath}")
+    String getFiles(@PathVariable String filePath) {
+        log.info("getFiles() - filePath: [{}]", filePath);
+        return "filePath: " + filePath;
     }
 }
