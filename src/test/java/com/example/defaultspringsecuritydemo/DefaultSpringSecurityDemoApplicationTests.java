2219c8611422cae07895dde6fa8558b667aa6572
Josip Matijaš <josip.matijas@gmail.com>
2026-04-27 17:51:01 +0200

    Initial commit

---
+++ src/test/java/com/example/defaultspringsecuritydemo/DefaultSpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.defaultspringsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultSpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
