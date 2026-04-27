f7c0498235ada41ab002210f8d3c2f4f538a48c4
Josip Matijaš <josip.matijas@gmail.com>
2026-04-28 01:01:36 +0200

    Integration tests; Starting web server at random port

--- src/test/java/com/example/defaultspringsecuritydemo/DefaultSpringSecurityDemoApplicationTests.java
+++ src/test/java/com/example/defaultspringsecuritydemo/DefaultSpringSecurityDemoApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.defaultspringsecuritydemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DefaultSpringSecurityDemoApplicationTests {
 
+    TestRestTemplate restTemplate;
+    URL base;
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        restTemplate = new TestRestTemplate("user", "password");
+        base = URI.create("http://localhost:" + port).toURL();
+    }
+
     @Test
-    void contextLoads() {
+    public void whenLoggedUserRequestsHomePage_ThenSuccess()
+            throws IllegalStateException, IOException {
+        ResponseEntity<String> response =
+                restTemplate.getForEntity(base.toString(), String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(response.getBody().contains("Hello World!"));
+    }
+
+    @Test
+    public void whenUserWithWrongCredentials_thenUnauthorizedPage()
+            throws Exception {
+
+        restTemplate = new TestRestTemplate("user", "wrongpassword");
+        ResponseEntity<String> response =
+                restTemplate.getForEntity(base.toString(), String.class);
+
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+        assertTrue(response.getBody().contains("Unauthorized"));
     }
 
 }
